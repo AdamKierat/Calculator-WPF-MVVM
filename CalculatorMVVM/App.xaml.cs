dc9fcceaf69f6cbcb88de0842f8b28abce0796b1
VeeZ <adam.kierat1@gmail.com>
2020-03-23 01:59:02 +0100

    Window shows

--- CalculatorMVVM/App.xaml.cs
+++ CalculatorMVVM/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using CalculatorMVVM.App_Start;
 
 namespace CalculatorMVVM
 {
@@ -13,5 +14,9 @@ namespace CalculatorMVVM
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            new BootStrapper().Run();
+        }
     }
 }
---
+++ CalculatorMVVM/App_Start/BootStrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CalculatorMVVM.Views;
+using Prism.Unity;
+
+
+
+namespace CalculatorMVVM.App_Start
+{
+    public class BootStrapper:UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
